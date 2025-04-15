4b5017f5f6419fbe97a7fd637a16a1436a869e5a
Carlos dos anjos <78938222+ILord1099@users.noreply.github.com>
2025-04-14 22:00:07 -0300

    Movement

--- 2DPixel/Assets/Script/PlayerController.cs
+++ 2DPixel/Assets/Script/PlayerController.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
+     Vector2 vel;
+     public float speed;
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       Move();
+    }
+
+
+
+    public void Move()
+    {
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); 
+        rb.linearVelocity = speed * vel;
+          
     }
 }
